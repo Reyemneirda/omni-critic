a9cc9bf89390d1ea828005f125a21e5073447638
Reyemneirda <reyemneirda@gmail.com>
2020-07-07 20:06:53 +0300

    Add info to Article model

--- server/models/article.js
+++ server/models/article.js
@@ -6,16 +6,29 @@ var ArticleSchema = new Schema({
   title: String, // String is shorthand for {type: String}
   author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   body: String,
   comments: [{ body: String, date: Date }],
-  date: { type: Date, default: Date.now },
   hidden: Boolean,
   meta: {
     votes: Number,
-    favs: Number
-  }
+    favs: Number,
+  },
+  slug: String,
+  description: String,
+  tagList: [
+    {
+      type: String,
+    },
+  ],
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: Date,
+  favorited: Boolean,
+  favoritesCount: Number,
 });
-
+ArticleSchema.post("save", function (doc) {
+  updatedAt = Date.now;
+  slug = title.toLowerCase().replace(name.replace(/\s/g, "-"));
+});
 module.exports = mongoose.model("Article", ArticleSchema);
